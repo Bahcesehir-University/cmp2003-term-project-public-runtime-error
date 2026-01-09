6433fc3e9670702df817da91b510012b9d61b427
KubilayKaraca <sabriemrekubilay.kar@bahcesehir.edu.tr>
2026-01-09 18:59:43 +0300

    Update analyzer.h

--- analyzer.h
+++ analyzer.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <string>
 #include <vector>
-
+#include <unordered_map>
+#include <algorithm>
 struct ZoneCount {
     std::string zone;
     long long count;
@@ -20,7 +21,14 @@ public:
 
     // Top K zones: count desc, zone asc
     std::vector<ZoneCount> topZones(int k = 10) const;
+     
 
     // Top K slots: count desc, zone asc, hour asc
     std::vector<SlotCount> topBusySlots(int k = 10) const;
+
+    static  bool cmpZone(const ZoneCount& a,const ZoneCount& b);   
+    static bool cmpSlot(const SlotCount& a,const SlotCount& b);
+private:
+    std::unordered_map<std::string, long long> zone_counter;  //def for zone counter we defined here because we are gonna use them also in the other functions
+    std::unordered_map<std::string, long long> slot_counter;  //def for slot counter
 };
